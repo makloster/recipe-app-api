5b6416b799d982081eb9ac40482185d17ec4069a
makloster <klostermanuelandres@gmail.com>
2024-08-05 16:29:11 -0300

    wip/implement custom user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -18,18 +18,16 @@ class AdminSiteTests(TestCase):
             password='testpass123',
         )
         self.client.force_login(self.admin_user)
-        self.admin_user = get_user_model().objects.create_user(
+        self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
             name='Test User',
         )
 
-        def test_users_list(self):
-            """Test that users are listed on page"""
-            url = reverse('admin:core_user_changelist')
-            res = self.client.get(url)
+    def test_users_list(self):
+        """Test that users are listed on page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-            self.assertContains(res, self.user.name)
-            self.assertContains(res, self.user.email)
-
-            return super().setUp()
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
